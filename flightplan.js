8fb18657ae0a54f9df37f68fd21028ee4bf77c8f
rstar2 <rstar2@abv.bg>
2015-11-02 15:18:40 +0200

    a

--- flightplan.js
+++ flightplan.js
@@ -49,11 +49,28 @@ plan.remote(function(remote) {
   remote.sudo('cp -R /tmp/' + tmpDir + ' ~', {user: username});
   remote.rm('-rf /tmp/' + tmpDir);
 
-  remote.log('Install dependencies');
+  remote.log('Install dependencies');	
   remote.sudo('npm --production --prefix ~/' + tmpDir + ' install ~/' + tmpDir, {user: username});
 
   remote.log('Reload application');
   remote.sudo('ln -snf ~/' + tmpDir + ' ~/'+appName, {user: username});
+  // 1. using forever module
   remote.exec('forever stop ~/'+appName+'/'+startFile, {failsafe: true});
   remote.exec('forever start ~/'+appName+'/'+startFile);
+  
+  // 2. using a native service - but 2 thing have to be done first to make this :
+  // 2.1. A configuration file 'node-app.conf' must be added in the '/etc/init' directory
+  //		start on filesystem and started networking
+  //		respawn
+  //		chdir /home/deploy/node-app
+  //		env NODE_ENV=production #change this to staging if this is a staging server
+  //		env PORT=3000
+  //		exec /usr/local/bin/node bin/www
+  // 2.2 also the deplaying user 'username' should have bben given passwordless sudo access with
+  //   		echo "deploy ALL=(root) NOPASSWD: /sbin/restart node-app" >> /etc/sudoers
+  // remote.exec('sudo restart node-app');
 });
+
+
+// RUN it with
+// fly production 
