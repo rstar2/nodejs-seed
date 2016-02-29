6d54643220010cd4c1e3d35a0a8ec0defb2bd29e
rstar2 <cadb12new>
2016-02-29 23:38:37 +0200

    Replce Jade with Handlebars

--- config/express.js
+++ config/express.js
@@ -21,9 +21,6 @@ module.exports = function (app, config) {
   app.locals.ENV = env;
   app.locals.ENV_DEVELOPMENT = env == 'development';
 
-  app.set('views', config.root + '/app/views');
-  app.set('view engine', 'jade');
-
   var hbs = exphbs.create({
     layoutsDir: config.root + '/app/views/layouts/',
     defaultLayout: 'main',
